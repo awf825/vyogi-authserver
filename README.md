b9bc227108a46284842fa4079dd1fe5d3bdd44d7
awf825 <faiden454@gmail.com>
2021-01-30 20:50:15 -0500

    booking is being created with user and lesson updated as well

--- README.md
+++ README.md
@@ -9,3 +9,26 @@
    ```
    var getAllIds = db.getCollection('x').find({}, {_id:1}).map(function(item){ return item._id; })
    ```
+
+   For big file seeding (mongo shell):
+   ```
+   var file = cat('./schemas/lessons/Sat\ Jan\ 30\ 2021\ 15:00:44\ GMT-0500\ \(Eastern\ Standard\ Time\).json');
+
+   var o = JSON.parse(file);
+
+   db.lessons.insert(o);
+
+   db.lessons.save()
+   ```
+
+   Something like this will update a nested array that already has at least one entry. $push is a very cool command, because you can also attach nested attrs to a document on the fly i.e a custom attr.  
+   ```
+   User.update({"email": "client1@clientmail.com"}, {$push: {bookings: {bookingId: "bkg", lessonId: "lsn"}}});
+
+   User.update({"email": "client1@clientmail.com"}, {$push: {friends: {bookingId: "bkg", lessonId: "lsn"}}});
+   ```
+
+   Something like this will update a nested array that doesn't have an entries
+   ```
+   User.update({"email": "client1@clientmail.com"}, {$addToSet: {bookings: {bookingId: "bkg", lessonId: "lsn"}}});
+   ``` 
--- controllers/charge.js
+++ controllers/charge.js
@@ -1,6 +1,8 @@
 const mongoose = require('mongoose');
 const config = require('../config.js')
 const Booking = require('../models/booking.js')
+const User = require('../models/user.js')
+const Lesson = require('../models/lesson.js')
 
 exports.serveToken = function(req, res, next) {
 	res.json(config.stripePublishableKeyTest)
@@ -11,6 +13,14 @@ exports.charge = async function(req, res, next) {
 	const stripe = Stripe(config.stripeSecretKeyTest);
 	const lesson = req.body.lesson;
 	const user = req.body.user;
+	const userQuery = { "_id": user };
+	const lessonQuery = { "_id": lesson };
+	const newBooking = new Booking({
+		payment_made: true,
+		cancelled: false,
+		userId: user,
+		lessonId: lesson
+	});
 
 	try {
 		charge = await stripe.charges.create({
@@ -19,24 +29,37 @@ exports.charge = async function(req, res, next) {
 	  		source: req.body.token,
 	  		description: 'My Test Charge API docs',
 		});
-
 	} catch (err) {
-		res.sendStatus(500)
+		res.send(500, { message: 'broke on stripe' })
 	}
 
-	const newBooking = new Booking({
-		payment_made: true,
-		cancelled: false,
-		userId: user,
-		lessonId: lesson
-	});
-
-	newBooking.save(function (err) {
+	await newBooking.save(function (err, booking) {
 		if (err) return handleError(err);
-		// INJECT USER AND LESSON FORM PAYLOAD
-		// INTO BOOKING
+		User.findOneAndUpdate(
+			userQuery, 
+			{ 
+				"$push": { 
+					"bookings": { 
+						"_id": booking._id, 
+						"lessonId": mongoose.Types.ObjectId(lesson) 
+					} 
+				} 
+			}, function(err, doc) {
+				if (err) return res.send(500, {error: err});
+			} 
+		);
+		Lesson.findOneAndUpdate(
+			lessonQuery, 
+			{ 
+				"$push": { 
+					"bookings": { 
+						"_id": booking._id, 
+						"userId": mongoose.Types.ObjectId(user) 
+					} 
+				} 
+			}, function(err, doc) {
+				if (err) return res.send(500, {error: err});
+			} );
 		res.sendStatus(204);
 	});
-
-  	
 }
