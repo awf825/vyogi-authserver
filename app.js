447ecd7dbd0edf0992e60b16d4004d8aa65f57f5
awf825 <faiden454@gmail.com>
2021-02-10 22:02:04 -0500

    enabled cors

--- app.js
+++ app.js
@@ -21,7 +21,19 @@ try {
 	console.log("could not connect, error:", error);    
 }
 
-app.use(cors());
+const allowedOrigins = ['http://localhost:3000', 'https://www.yogastaging.net']
+app.use(cors({
+	origin: function(origin, callback) {
+		// allow requests with no origin (like mobile apps or curl requests)
+	    if(!origin) return callback(null, true);
+
+	    if(allowedOrigins.indexOf(origin) === -1) {
+	    	var msg = 'The CORS policy for this site does not allow access from the specified origin.';
+	    	return callback(new Error(msg), false);
+	    }
+	    return callback(null, true);
+	}
+}));
 app.use(bodyParser.json({ type: '*/*' }));
 
 router(app);
