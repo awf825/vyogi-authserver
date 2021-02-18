0822ddfb8b5416d4cd86a632a8446a59bac0259f
awf825 <faiden454@gmail.com>
2021-02-17 20:49:06 -0500

    ses wired up in charges controller and sending email

--- controllers/charge.js
+++ controllers/charge.js
@@ -2,9 +2,19 @@ const mongoose = require('mongoose');
 const Booking = require('../models/booking.js')
 const User = require('../models/user.js')
 const Lesson = require('../models/lesson.js')
+const AWS = require('aws-sdk')
 // const webEnvs = ['beta', 'production']
 const pubKey = process.env.STRIPE_PUBLISHABLE_KEY_TEST;
 const stripeSecret = process.env.STRIPE_SECRET_KEY_TEST;
+const accessKey = process.env.AWS_ACCESS_KEY;
+const secretKey = process.env.AWS_SECRET;
+
+const SESConfig = {
+	apiVersion: '2010-12-01',
+	accessKeyId: accessKey,
+	secretAccessKey: secretKey,
+	region: 'us-east-1'
+}
 
 exports.serveToken = function(req, res, next) {
 	res.json(pubKey)
@@ -17,6 +27,7 @@ exports.charge = async function(req, res, next) {
 	const user = req.body.user;
 	const userQuery = { "_id": user };
 	const lessonQuery = { "_id": lesson };
+
 	const newBooking = new Booking({
 		payment_made: true,
 		cancelled: false,
@@ -66,6 +77,34 @@ exports.charge = async function(req, res, next) {
 				if (err) return res.send(500, {error: err});
 			} 
 		);
+
+		const emailParams = {
+			Source: 'no-reply@yogastaging.net',
+			Destination: {
+				ToAddresses: [
+					'faiden454@gmail.com'
+				]
+			},
+			Message: {
+				Body: {
+					Html: {
+						Charset: 'UTF-8',
+						Data: `${code}`
+					}
+				},
+				Subject: {
+					Charset: 'UTF-8',
+					Data: 'Hello Big Joe'
+				}
+			}
+		};
+
+		new AWS.SES(SESConfig).sendEmail(emailParams).promise().then((res) => {
+			console.log(res)
+		}).catch(err => {
+			console.log('error with ses:', err)
+		})
+
 		res.sendStatus(204);
 	});
 }
